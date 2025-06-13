842644fe16e3c5a93d04640f0521fa4f9d495dbb
Prem2310 <115456546+Prem2310@users.noreply.github.com>
2025-06-13 08:56:14 +0000

    manifest changes

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -21,6 +21,12 @@ export default function manifest(): MetadataRoute.Manifest {
     scope: "/",
     screenshots: [
       {
+        src: "https://res.cloudinary.com/dkvsiuokl/image/upload/v1749237116/profile_b716fj.jpg",
+        type: "image/webp",
+        sizes: "440x956",
+        form_factor: "narrow",
+      },
+      {
         src: "https://res.cloudinary.com/dkvsiuokl/image/upload/v1749453371/2025-06-09_12-45_x2lv56.png",
         type: "image/webp",
         sizes: "440x956",
